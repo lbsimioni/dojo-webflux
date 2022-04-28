40f2c5cda7ec8f19da6f181ff6f7f16f1af7b7cc
Leonardo Bucioli Simioni <t1001lnd@pestadorcbmp.com.br>
2022-04-28 12:46:29 -0300

    Added TODOS

---
+++ src/test/java/br/com/dojo/webflux/controllers/CourseControllerTest.java
@@ -0,0 +1,9 @@
+package br.com.dojo.webflux.controllers;
+
+// TODO: Adicionar Configurações
+public class CourseControllerTest {
+
+    // TODO: Adicionar testes para todas as rotas da controller
+
+
+}
---
+++ src/test/java/br/com/dojo/webflux/services/CourseServiceTest.java
@@ -0,0 +1,6 @@
+package br.com.dojo.webflux.services;
+
+public class CourseServiceTest {
+
+    // TODO: Adicionar Testes para as chamadas do service
+}
